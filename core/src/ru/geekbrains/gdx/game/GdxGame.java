dfc9bee9cd362b111042bce3a81ac4f6eca23aea
KolunovYuriy <y.kolunov@gmail.com>
2021-10-17 22:12:15 +0300

    homework1 new background

--- core/src/ru/geekbrains/gdx/game/GdxGame.java
+++ core/src/ru/geekbrains/gdx/game/GdxGame.java
@@ -1,25 +1,32 @@
 package ru.geekbrains.gdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture img, backfroundSpace;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		backfroundSpace = new Texture(Gdx.files.internal("space.jpg"));
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		TextureRegion t = ScreenUtils.getFrameBufferTexture();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(backfroundSpace,0, 0, t.getRegionWidth(), t.getRegionHeight());
+		//batch.draw(backfroundSpace,0, 0 ,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
