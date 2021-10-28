3ee759d9467477fa413576f88e6c87022e7ea7cb
KolunovYuriy <y.kolunov@gmail.com>
2021-10-29 00:59:43 +0300

    homework2 task

--- core/src/ru/geekbrains/gdx/game/screen/MenuScreen.java
+++ core/src/ru/geekbrains/gdx/game/screen/MenuScreen.java
@@ -8,24 +8,24 @@ import ru.geekbrains.gdx.game.base.BaseScreen;
 public class MenuScreen extends BaseScreen {
 
     private Texture img;
-    private Vector2 touch;
-    private Vector2 v;
+    private Vector2 imgPosition, touch, v;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
+        imgPosition = new Vector2(0,0);
         touch = new Vector2();
-        v = new Vector2(1, 1);
+        v = new Vector2(0, 0);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-        batch.draw(img, touch.x, touch.y);
+        batch.draw(img, imgPosition.x, imgPosition.y);
         batch.end();
-        touch.add(v);
+        if (imgPosition.dst(touch) > v.len()) imgPosition.add(v);
     }
 
     @Override
@@ -37,6 +37,7 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(imgPosition)).setLength(1);
         return super.touchDown(screenX, screenY, pointer, button);
     }
 
